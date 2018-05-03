77c81f13ea16d2d99e9eacb580c7a34c2e4789d2
Miguel Villamil <miavillamilca@unal.edu.co>
2018-05-03 06:42:35 -0500

    ...

--- src/auth/server.js
+++ src/auth/server.js
@@ -1,3 +1,3 @@
 export const url = process.env.AUTH_URL || 'login-ms';
-export const port = process.env.AUTH_PORT || '4000';
+export const port = process.env.AUTH_PORT || '6004';
 export const entryPoint = process.env.AUTH_ENTRY || 'ldap';
