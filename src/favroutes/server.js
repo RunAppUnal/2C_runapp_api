3a202b8b41efe9ccbc17a6a004108df8cd7922b6
Christian Camilo Vaca Daza <ccvacad@unal.edu.co>
2018-05-07 01:11:51 -0500

    Update port

--- src/favroutes/server.js
+++ src/favroutes/server.js
@@ -1,3 +1,3 @@
 export const url = process.env.FAVROUTES_URL || 'vehicles-ms';
-export const port = process.env.FAVROUTES_PORT || '4003';
+export const port = process.env.FAVROUTES_PORT || '6005';
 export const entryPoint = process.env.FAVROUTES_ENTRY || 'fav_routes';
